9dd80aeaf5806a81bc6dc336a099d0293e3aff09
Sayed Moin Ahmed <sayed.ahmed@nextory.com>
2020-11-08 21:09:06 +0530

    Local Variable

---
+++ src/main/java/com/java15/example/localvariable/Application.java
@@ -0,0 +1,11 @@
+package com.java15.example.localvariable;
+
+/**
+ * https://docs.oracle.com/en/java/javase/11/language/local-variable-type-inference.html
+ * https://openjdk.java.net/projects/amber/LVTIstyle.html
+ */
+public class Application {
+    public static void main(String[] args){
+
+    }
+}
