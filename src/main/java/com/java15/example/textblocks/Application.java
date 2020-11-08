bedfe8200d719c65a561454ccf784e38757f7107
Sayed Moin Ahmed <sayed.ahmed@nextory.com>
2020-11-08 12:34:50 +0530

    text block

--- src/main/java/com/java15/example/textblocks/Application.java
+++ src/main/java/com/java15/example/textblocks/Application.java
@@ -10,9 +10,25 @@ public class Application {
      * @param args
      */
     public static void main(String[] args){
+        demo();
+
+
+    }
+
+    private static void equals(){
+        String str ="Hello World";
+        String txt = """
+                Hello World""";
+        System.out.println("=="+(str==txt));
+        System.out.println("equals"+str.equals(txt));
+        System.out.println("equals"+(str.length()==txt.length()));
+    }
+
+    private static void demo() {
         String textBlocks= """
-                Hello world""";
-        System.out.println(textBlocks);
+                Hello world %s""";
+        String name = "Demo";
+        System.out.println(String.format(textBlocks,name));
     }
 
 
