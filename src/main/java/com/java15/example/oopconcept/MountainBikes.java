1e810aec8f1120557d67b3cf2bc1c0fd4456960e
Sayed Moin Ahmed <sayed.ahmed@nextory.com>
2020-11-10 08:39:39 +0530

    Hierarchy

---
+++ src/main/java/com/java15/example/oopconcept/MountainBikes.java
@@ -0,0 +1,20 @@
+package com.java15.example.oopconcept;
+
+/**
+ * https://docs.oracle.com/javase/tutorial/java/concepts/inheritance.html
+ */
+public class MountainBikes extends Bicycle{
+
+    protected MountainBikes(String owner){
+        this.owner = owner;
+    }
+
+    public String show(){
+        String msg = """
+                This is MountainBike...
+                """;
+        System.out.println(msg);
+        return msg;
+    }
+
+}
