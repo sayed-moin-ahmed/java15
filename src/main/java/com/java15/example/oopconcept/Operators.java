9da7491e0e1d7947ba6533bc58bd404d6443f574
Sayed Moin Ahmed <sayed.ahmed@nextory.com>
2020-11-19 19:50:25 +0530

    Arrays

---
+++ src/main/java/com/java15/example/oopconcept/Operators.java
@@ -0,0 +1,11 @@
+package com.java15.example.oopconcept;
+
+public class Operators {
+    public static void main(String[] args){
+        Integer a = 5;
+        Integer b = 7;
+        System.out.println(a&b);
+        System.out.println(a|b);
+        //System.out.println(a&&b);
+    }
+}
