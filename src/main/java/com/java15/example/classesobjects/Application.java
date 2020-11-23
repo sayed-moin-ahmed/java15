52f605e3e44758019dd6029af38a30b5d8158eeb
Sayed Moin Ahmed <sayed.ahmed@nextory.com>
2020-11-23 11:03:25 +0530

    Object individual copy of fields

--- src/main/java/com/java15/example/classesobjects/Application.java
+++ src/main/java/com/java15/example/classesobjects/Application.java
@@ -16,6 +16,17 @@ public class Application {
         reference = new FunctionalInterfaceImpl();
         reference.toString();
         ((FunctionalInterfaceImpl) reference).ni.show1();
+        IdentifyMyParts a = new IdentifyMyParts();
+        IdentifyMyParts b = new IdentifyMyParts();
+        a.y = 5;
+        b.y = 6;
+        a.x = 1;
+        b.x = 2;
+        System.out.println("a.y = " + a.y);
+        System.out.println("b.y = " + b.y);
+        System.out.println("a.x = " + a.x);
+        System.out.println("b.x = " + b.x);
+        System.out.println("IdentifyMyParts.x = " + IdentifyMyParts.x);
     }
 
     private static TopInterface getTopInterface1() {
---
+++ src/main/java/com/java15/example/classesobjects/IdentifyMyParts.java
@@ -0,0 +1,7 @@
+package com.java15.example.classesobjects;
+
+public class IdentifyMyParts {
+    public static int x = 7;
+    public int y = 3;
+}
+
