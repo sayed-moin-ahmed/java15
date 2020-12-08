192bedd7b22480f47aa98d43b1453721773cce02
Sayed Moin Ahmed <sayed.ahmed@nextory.com>
2020-12-08 08:46:38 +0530

    Create Application.java

---
+++ src/main/java/com/java15/example/interfacesandinheritance/Application.java
@@ -0,0 +1,10 @@
+package com.java15.example.interfacesandinheritance;
+
+/**
+ * https://docs.oracle.com/javase/tutorial/java/IandI/index.html
+ */
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
