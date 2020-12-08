b19ea5edb37b8c71578c06494ff9a95de2b9f639
Sayed Moin Ahmed <sayed.ahmed@nextory.com>
2020-12-08 08:13:00 +0530

    Update Author.java

--- src/main/java/com/java15/example/annotations/Author.java
+++ src/main/java/com/java15/example/annotations/Author.java
@@ -1,8 +1,12 @@
 package com.java15.example.annotations;
 
-import java.lang.annotation.Documented;
+import java.lang.annotation.*;
 
+/**
+ * https://docs.oracle.com/javase/tutorial/java/annotations/predefined.html
+ */
 @Documented
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Author {
     String name();
     String date();
