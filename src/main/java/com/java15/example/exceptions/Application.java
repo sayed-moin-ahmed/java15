97a7542a398043dc7f8045b351a6ef7c240da3e0
Sayed Moin Ahmed <sayed.ahmed@nextory.com>
2020-12-22 15:47:22 +0530

    return statment

--- src/main/java/com/java15/example/exceptions/Application.java
+++ src/main/java/com/java15/example/exceptions/Application.java
@@ -6,16 +6,63 @@ package com.java15.example.exceptions;
 public class Application {
     public static void main(String[] args) {
         // exceptionCallStack();
-        tryCatch();
+        // tryCatch();
+        System.out.println(returnStatementWithExceptionDemo());
     }
 
     //Catching More Than One Type of Exception with One Exception Handler
+    /**
+     * Note: If a catch block handles more than one exception type, then the catch parameter is implicitly final.
+     * In this example, the catch parameter ex is final and therefore you cannot assign any values to it within the catch block.
+     */
     private static void tryCatch() {
         try{
             exceptionCallStack();
             throw new ArrayIndexOutOfBoundsException("Array Exception");
         }catch (ArrayIndexOutOfBoundsException|IllegalArgumentException throwable){
             System.out.println(throwable.getMessage());
+        }
+    }
+
+    private static String returnStatement(){
+        try{
+            return "Hello World!";
+        }catch (Exception e) {
+            System.out.println("Exception...");
+            return e.getMessage();
+        }
+    }
+
+    //finally will override the return of normal try block
+    private static String returnStatmentFinallyDemo(){
+        try{
+            return "Hello World!";
+        }catch (Exception e){
+            System.out.println("Exception...");
+            return e.getMessage();
+        }finally {
+            return "finally...";
+        }
+    }
+
+    private static String returnStatementWithExceptionDemo(){
+        try{
+            throw new ArrayIndexOutOfBoundsException();
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("ArrayIndexOutOfBoundsException");
+            return e.getMessage();
+        }
+    }
+
+    //finally will override the return of catch block
+    private static String returnStatementWithExceptionFinallyDemo(){
+        try{
+            throw new ArrayIndexOutOfBoundsException();
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("ArrayIndexOutOfBoundsException");
+            return e.getMessage();
+        }finally {
+            return "finally...";
         }
     }
 
