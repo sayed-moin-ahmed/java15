9fca99f525bc5190676d3c37251919d6759eba9f
Sayed Moin Ahmed <sayed.ahmed@nextory.com>
2020-12-22 14:24:32 +0530

    Exception Handling

---
+++ src/main/java/com/java15/example/exceptions/Application.java
@@ -0,0 +1,11 @@
+package com.java15.example.exceptions;
+
+/**
+ * https://docs.oracle.com/javase/tutorial/essential/exceptions/catchOrDeclare.html
+ */
+public class Application {
+    public static void main(String[] args) {
+        Caller caller = new Caller();
+        caller.call();
+    }
+}
