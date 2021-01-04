7dda5d3e340d86d8d5d7e4d9ba19c9d483aebfd3
Sayed Moin Ahmed <sayed.ahmed@nextory.com>
2021-01-04 08:55:17 +0530

    Java specifications...

--- src/main/java/com/java15/example/ExampleApplication.java
+++ src/main/java/com/java15/example/ExampleApplication.java
@@ -3,6 +3,11 @@ package com.java15.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * https://docs.oracle.com/javase/tutorial/index.html
+ * https://docs.oracle.com/en/java/javase/15/docs/api/java.base/module-summary.html
+ * https://docs.oracle.com/javase/specs/index.html
+ */
 @SpringBootApplication
 public class ExampleApplication {
 
