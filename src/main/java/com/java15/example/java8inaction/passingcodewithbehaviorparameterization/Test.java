5c44fde91b654ba565609273d69d28d42ef78e99
Sayed Moin Ahmed <sayed.moin.85@gmail.com>
2021-03-06 16:44:19 +0530

    Generic Lambda

--- src/main/java/com/java15/example/java8inaction/passingcodewithbehaviorparameterization/Test.java
+++ src/main/java/com/java15/example/java8inaction/passingcodewithbehaviorparameterization/Test.java
@@ -6,12 +6,30 @@ import com.java15.example.java8inaction.Data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class Test {
     public static void main(String[] args) {
         List<Apple> apples = Data.getData();
-        beforeJava8(apples);
+        // beforeJava8(apples);
+        //filter(apples,(Apple e) -> Colors.GREEN.equals(e.getColor())).forEach(System.out::println);
+        groupBy(apples,apple -> apple.getWeight()).forEach((i, list)->System.out.println(i+""+list));
     }
+
+    //More Generic
+    static public<T> List<T> filter(List<T> list,Predicate<T> predicate){
+        return list.stream().filter(predicate).collect(Collectors.toList());
+    }
+
+    static public <T,K> Map<? extends K, List<T>> groupBy(List<T> list, Function<? super T, ? extends K> classifier){
+        return  list.stream().collect(groupingBy(classifier));
+    }
+
     private static void beforeJava8(List<Apple> apples) {
         final List<Apple> filteredApple = new ArrayList<>();
         apples.forEach(e->
