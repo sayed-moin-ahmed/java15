0e8d57d21b4bc1a1d718c822d0e338e14d020356
Sayed Moin Ahmed <s3dm2na3d@gmail.com>
2021-03-06 15:36:50 +0530

    min and max

--- src/main/java/com/java15/example/java8inaction/Java8_9_10_11_whatshappening/Test.java
+++ src/main/java/com/java15/example/java8inaction/Java8_9_10_11_whatshappening/Test.java
@@ -1,10 +1,7 @@
 package com.java15.example.java8inaction.Java8_9_10_11_whatshappening;
 
 import java.io.File;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -13,13 +10,14 @@ import static java.util.stream.Collectors.groupingBy;
 public class Test {
 
     public static void main(String[] args) {
-        List<Apple>  apples = getData();
+        //List<Apple>  apples = getData();
         //filterApples(apples,(Apple a) -> Colors.GREEN.equals(a.getColor())).stream().forEach(System.out::println);
         //filterApples(apples,(Apple a) -> Colors.GREEN.equals(a.getColor())).stream().forEach(System.out::println);
         //filterGroupApples(apples,(Apple a) -> Colors.RED.equals(a.getColor())).forEach((i,list) ->{System.out.println(i+""+list);});
         //filterGroupApples(apples,(Apple a) -> {return true;}).forEach((i,list) ->{System.out.println(i+""+list);});
-        minAppleByWeight(apples).ifPresent(System.out::println);
-        maxAppleByWeight(apples).ifPresent(System.out::println);
+        //minAppleByWeight(apples).ifPresent(System.out::println);
+        //maxAppleByWeight(apples).ifPresent(System.out::println);
+        getData1().forEach(System.out::println);
     }
 
     static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p) {
@@ -46,6 +44,24 @@ public class Test {
         Apple apple5 = new Apple(3,Colors.RED);
         Apple apple6 = new Apple(5,Colors.GREEN);
         List<Apple> apples =  List.of(apple1,apple2,apple3,apple4,apple5,apple6);
+        return apples;
+    }
+
+    private static List<Apple> getData1() {
+        Apple apple1 = new Apple(10,Colors.GREEN);
+        Apple apple2 = new Apple(1,Colors.RED);
+        Apple apple3 = new Apple(5,Colors.GREEN);
+        Apple apple4 = new Apple(3,Colors.RED);
+        Apple apple5 = new Apple(3,Colors.RED);
+        Apple apple6 = new Apple(5,Colors.GREEN);
+        List<Apple> apples =  new ArrayList<>();
+        apples.add(apple1);
+        apples.add(apple2);
+        apples.add(apple3);
+        apples.add(apple4);
+        apples.add(apple5);
+        apples.add(apple6);
+        apples.sort(Comparator.comparing(Apple::getWeight));
         return apples;
     }
 
