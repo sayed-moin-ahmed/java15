525e16c4941c7679fc050b88bb644088205f90ac
Sayed Moin Ahmed <s3dm2na3d@gmail.com>
2021-03-06 14:54:12 +0530

    Group By

--- src/main/java/com/java15/example/java8inaction/Java8_9_10_11_whatshappening/Test.java
+++ src/main/java/com/java15/example/java8inaction/Java8_9_10_11_whatshappening/Test.java
@@ -2,24 +2,44 @@ package com.java15.example.java8inaction.Java8_9_10_11_whatshappening;
 
 import java.io.File;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class Test {
 
     public static void main(String[] args) {
+        List<Apple>  apples = getData();
+      //  filterApples(apples,(Apple a) -> Colors.GREEN.equals(a.getColor())).stream().forEach(System.out::println);
+       // filterApples(apples,(Apple a) -> Colors.GREEN.equals(a.getColor())).stream().forEach(System.out::println);
+         filterGroupApples(apples,(Apple a) -> Colors.RED.equals(a.getColor())).forEach((i,list) ->{System.out.println(i+""+list);});
+
+    }
+
+    static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p) {
+        return inventory.stream().filter(p).collect(Collectors.toList());
+    }
+
+    static Map<Integer, List<Apple>> filterGroupApples(List<Apple> inventory, Predicate<Apple> p) {
+        return inventory.stream().filter(p).collect(groupingBy(Apple::getWeight));
+    }
+
+    private static List<Apple> getData() {
         Apple apple1 = new Apple(10,Colors.GREEN);
         Apple apple2 = new Apple(1,Colors.RED);
         Apple apple3 = new Apple(5,Colors.GREEN);
         Apple apple4 = new Apple(3,Colors.RED);
-        Apple apple5 = new Apple(4,Colors.GREEN);
-        List<Apple> apples =  List.of(apple1,apple2,apple3,apple4,apple5);
-
-
+        Apple apple5 = new Apple(3,Colors.RED);
+        Apple apple6 = new Apple(4,Colors.GREEN);
+        List<Apple> apples =  List.of(apple1,apple2,apple3,apple4,apple5,apple6);
+        return apples;
     }
 
-    File[] handlingFiles(){
+    static File[] handlingFiles(){
         File[] hiddenFiles = new File(".").listFiles(File::isHidden);
         return hiddenFiles;
     }
-
 
 }
--- src/main/java/com/java15/example/java8inaction/Test.java
+++ src/main/java/com/java15/example/java8inaction/Test.java
@@ -4,6 +4,24 @@ public class Test {
 
     public static void main(String[] args){
 
+        int counter = 0;
+        int temp;
+        //9,3,6,5,7,2
+        int[] arr ={10,9,3,6,5,7,2,13};
+        for(int i= 0; i<arr.length;i++){
+            for(int j=1;j<arr.length;j++){
+                if(arr[i]<arr[j]){
+                    counter++;
+                    if(counter>2) {
+                        break;
+                    }
+                }
+            }
+
+        }
+        System.out.println(counter);
+
+
     }
 
 }
