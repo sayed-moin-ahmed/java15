260066edb6f1627c5afd4b74494b5bacd4c3f275
sayed-moin-ahmed <sayed.moin.85@gmail.com>
2021-03-19 16:37:17 +0530

    ch5ch5

--- src/main/java/com/java15/example/modern_java_in_action/ch5_workingwithstream/Test.java
+++ src/main/java/com/java15/example/modern_java_in_action/ch5_workingwithstream/Test.java
@@ -1,11 +1,15 @@
 package com.java15.example.modern_java_in_action.ch5_workingwithstream;
 
 import com.java15.example.modern_java_in_action.Dish;
+import com.java15.example.modern_java_in_action.Trader;
+import com.java15.example.modern_java_in_action.Transaction;
 import com.java15.example.modern_java_in_action.Type;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
@@ -16,9 +20,40 @@ public class Test {
                 new Dish("prawns", false, 300, Type.FISH),
                 new Dish("rice", true, 350, Type.OTHER),
                 new Dish("chicken", false, 400, Type.MEAT),
-                new Dish("french fries", true, 530, Type.OTHER));
+                new Dish("french fries", true, 530, Type.OTHER),
+                new Dish("french fries", true, 30, Type.OTHER),
+                new Dish("french fries", true, 530, Type.OTHER)
+        );
 
 
-        specialMenu.stream().filter(dish->dish.getCalories()<320).collect(toList()).forEach(System.out::println);
+        /*specialMenu.stream().filter(dish->dish.getCalories()<320).collect(toList()).forEach(System.out::println);
+        specialMenu.stream().dropWhile(dish->dish.getCalories()<320).collect(toList()).forEach(System.out::println);
+        specialMenu.stream().takeWhile(dish->dish.getCalories()<320).collect(toList()).forEach(System.out::println);
+        specialMenu.stream().filter(dish -> dish.getType() == Type.MEAT).limit(2).collect(toList());*/
+
+       /* String[] arrayOfWords = {"Goodbye", "World"};
+        Stream<String> streamOfwords = Arrays.stream(arrayOfWords);
+        streamOfwords.map(word -> word.split("")).flatMap(Arrays::stream).forEach( System.out::println);*/
+
+       /* List<Integer> numbers = Arrays.asList(1, 2, 3);
+        Optional<Integer> max = numbers.stream().reduce(Integer::max);
+        System.out.println(max.get());
+        Optional<Integer> min = numbers.stream().reduce(Integer::min);
+        System.out.println(min.get());*/
+
+
+
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario","Milan");
+        Trader alan = new Trader("Alan","Cambridge");
+        Trader brian = new Trader("Brian","Cambridge");
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction(brian, 2011, 300),
+                new Transaction(raoul, 2012, 1000),
+                new Transaction(raoul, 2011, 400),
+                new Transaction(mario, 2012, 710),
+                new Transaction(mario, 2012, 700),
+                new Transaction(alan, 2012, 950)
+        );
     }
 }
