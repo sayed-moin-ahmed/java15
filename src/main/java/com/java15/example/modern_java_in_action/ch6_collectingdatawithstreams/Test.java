089af2e3737a1e3e6152e29af7529af224374b12
sayed-moin-ahmed <sayed.moin.85@gmail.com>
2021-03-20 18:28:22 +0530

    Update Test.java

--- src/main/java/com/java15/example/modern_java_in_action/ch6_collectingdatawithstreams/Test.java
+++ src/main/java/com/java15/example/modern_java_in_action/ch6_collectingdatawithstreams/Test.java
@@ -3,11 +3,14 @@ package com.java15.example.modern_java_in_action.ch6_collectingdatawithstreams;
 import com.java15.example.modern_java_in_action.Dish;
 import com.java15.example.modern_java_in_action.Type;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.maxBy;
 
 public class Test {
     public static void main(String[] args){
@@ -18,11 +21,17 @@ public class Test {
                 new Dish(Type.INDIAN,"Butter Masala",500)
         );
 
-        Map<Boolean,List<Dish>> partitionBy = dishes.stream().collect(Collectors.partitioningBy(dish->Type.CHINESE==dish.getType()));
+        /*Map<Boolean,List<Dish>> partitionBy = dishes.stream().collect(Collectors.partitioningBy(dish->Type.CHINESE==dish.getType()));
         partitionBy.forEach((key, value) -> System.out.println(key + " " + value));
 
         Map<Type, List<Dish>> groupingBy = dishes.stream().collect(groupingBy(Dish::getType));
-        groupingBy.forEach((k,v)->System.out.println(k+" "+v));
+        groupingBy.forEach((k,v)->System.out.println(k+" "+v));*/
+
+
+        Optional<Dish> max1 = dishes.stream().max(Comparator.comparing(Dish::getCalories));
+        Optional<Integer> max2 = dishes.stream().map(Dish::getCalories).reduce(Integer::max);
+        System.out.println(max1 +""+max2);
+
     }
 
 }
